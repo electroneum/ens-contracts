66f63e5a1705be2ed53eed8b1a023dd5a4e03143
Jordan Butler <jordan.butler2@nhs.net>
2026-07-08 11:47:38 +0100

    Fix last remaining .eth reference in TestReverseRegistrar test

--- test/reverseRegistrar/TestReverseRegistrar.test.ts
+++ test/reverseRegistrar/TestReverseRegistrar.test.ts
@@ -333,7 +333,7 @@ describe('ReverseRegistrar', () => {
         dummyOwnable.address,
         accounts[0].address,
         publicResolver.address,
-        'dummyownable.eth',
+        'dummyownable.etn',
       ])
 
       await expect(
@@ -341,7 +341,7 @@ describe('ReverseRegistrar', () => {
       ).resolves.toEqualAddress(accounts[0].address)
       await expect(
         publicResolver.read.name([getReverseNodeHash(dummyOwnable.address)]),
-      ).resolves.toEqual('dummyownable.eth')
+      ).resolves.toEqual('dummyownable.etn')
     })
   })
 
